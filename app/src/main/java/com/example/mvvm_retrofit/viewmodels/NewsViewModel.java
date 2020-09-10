1dc36d40888981f3bfad3bf520a7642c1d880c34
= <=>
2020-09-10 14:52:26 +0300

    Update NewsViewModel.java

--- app/src/main/java/com/example/mvvm_retrofit/viewmodels/NewsViewModel.java
+++ app/src/main/java/com/example/mvvm_retrofit/viewmodels/NewsViewModel.java
@@ -1,6 +1,7 @@
 package com.example.mvvm_retrofit.viewmodels;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.mvvm_retrofit.models.Articles;
@@ -12,9 +13,22 @@ public class NewsViewModel extends ViewModel {
 
     NewsRepo newsRepo = new NewsRepo();
 
+    // news list
     public LiveData<List<Articles>> getNews() {
         return newsRepo.getNews();
     }
+
+    //click start
+    MutableLiveData<Articles> mutableArticles = new MutableLiveData<>();
+
+    public void setNewsDetail(Articles articles) {
+        mutableArticles.setValue(articles);
+    }
+
+    public LiveData<Articles> getNewDetail() {
+        return mutableArticles;
+    }
+    // end click
 
 
 }
